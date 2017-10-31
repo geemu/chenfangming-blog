4551ebb1a66dccea349d49f8cfdec14d8b42d033
chenfangming <chenfangming@91zdan.com>
2017-10-31 14:02:25 +0800

    mybatis

---
+++ pass_tool/src/main/java/me/geemu/persistence/dao/UserInfoDao.java
@@ -0,0 +1,7 @@
+package me.geemu.persistence.dao;
+
+import me.geemu.persistence.entity.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserInfoDao extends Mapper<UserInfo> {
+}
