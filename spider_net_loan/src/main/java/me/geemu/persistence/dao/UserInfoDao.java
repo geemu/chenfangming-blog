edcd10e173d435e6d5a61d84047f440d57779c7d
chenfangming <chenfangming@91zdan.com>
2017-11-15 15:47:32 +0800

    1

--- spider_net_loan/src/main/java/me/geemu/persistence/dao/UserInfoDao.java
+++
@@ -1,7 +0,0 @@
-package me.geemu.persistence.dao;
-
-import me.geemu.persistence.entity.UserInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserInfoDao extends Mapper<UserInfo> {
-}
