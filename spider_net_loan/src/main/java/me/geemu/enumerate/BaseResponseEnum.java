72a05a22f0af1ab396e8e57abacb98435172d682
chenfangming <chenfangming@91zdan.com>
2017-11-17 19:07:33 +0800

    1

--- spider_net_loan/src/main/java/me/geemu/enumerate/BaseResponseEnum.java
+++ spider_net_loan/src/main/java/me/geemu/enumerate/BaseResponseEnum.java
@@ -16,18 +16,6 @@ public enum BaseResponseEnum {
      */
     DEFAULT_OK(200, "success"),
     /**
-     * 未登录
-     */
-    DEFAULT_PLEASE_LOGIN(401, "请登录"),
-    /**
-     * 用户名或密码错误
-     */
-    DEAFULT_LOGIN_ERROR(701, "用户名或密码错误"),
-    /**
-     * 默认自定义异常
-     */
-    DEFAULT_BUSINESS_ERROR(201, "error occured"),
-    /**
      * 系统异常
      */
     DEFAULT_UNKNOW_ERROR(500, "系统维护中");
