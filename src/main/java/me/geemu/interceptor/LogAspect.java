81844015746bc14c8a4d589532199b4a6568906d
chenfangming <chenfangming@91zdan.com>
2017-12-18 18:20:42 +0800

    1

--- src/main/java/me/geemu/interceptor/LogAspect.java
+++ src/main/java/me/geemu/interceptor/LogAspect.java
@@ -43,6 +43,7 @@ public class LogAspect {
         logSb.append(String.format("[RequestURL]\t%s\r\n", attributes.getRequest().getRequestURL()));
         logSb.append(String.format("[RequestParams]\t%s\r\n", JSON.toJSONString(requestParams)));
         logSb.append(String.format("[TargetMethod]\t%s\r\n", targetMethod));
+        logSb.append(String.format("[ResponseCode]\t%s\r\n", attributes.getResponse().getStatus()));
         logSb.append(String.format("[ResponseBody]\t%s\r\n", JSON.toJSONString(response)));
         logger.info(logSb.toString());
         return response;
@@ -60,14 +61,19 @@ public class LogAspect {
         ErrorResponse response = new ErrorResponse();
         response.setMessage(e.getMessage());
         if (e instanceof UnAuthorizedException) {
+            attributes.getResponse().setStatus(HttpStatus.UNAUTHORIZED.value());
             response.setCode(HttpStatus.UNAUTHORIZED.value());
         } else if (e instanceof NotFoundException) {
+            attributes.getResponse().setStatus(HttpStatus.NOT_FOUND.value());
             response.setCode(HttpStatus.NOT_FOUND.value());
         } else if (e instanceof BusinessException) {
+            attributes.getResponse().setStatus(HttpStatus.BAD_REQUEST.value());
             response.setCode(HttpStatus.BAD_REQUEST.value());
         } else {
+            attributes.getResponse().setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
             response.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         }
+        logSb.append(String.format("[ResponseCode]\t%s\r\n", attributes.getResponse().getStatus()));
         logSb.append(String.format("[ResponseBody]\t%s\r\n", JSON.toJSONString(response)));
         if (e instanceof UnAuthorizedException || e instanceof BusinessException || e instanceof NotFoundException) {
             logger.warn(logSb.toString(), e);
