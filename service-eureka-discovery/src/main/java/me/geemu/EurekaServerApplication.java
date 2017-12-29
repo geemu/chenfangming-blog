05e1a9803f9fdab09968915491b71955aa2bba77
chenfangming <chenfangming@91zdan.com>
2017-12-29 13:46:41 +0800

    1

---
+++ service-eureka-discovery/src/main/java/me/geemu/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package me.geemu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Geemu
+ * Email: cfmmail@sina.com
+ * Date: 2017/12/5 13:42
+ * Description: 服务注册中心
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
