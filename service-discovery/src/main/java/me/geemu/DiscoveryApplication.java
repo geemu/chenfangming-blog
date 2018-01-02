b6ca38342940aeca4aa8c6ce0e8b147e3b6b4475
chenfangming <chenfangming@91zdan.com>
2018-01-02 12:34:40 +0800

    1

--- service-discovery/src/main/java/me/geemu/DiscoveryApplication.java
+++ service-discovery/src/main/java/me/geemu/DiscoveryApplication.java
@@ -2,19 +2,19 @@ package me.geemu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author Geemu
  * Email: cfmmail@sina.com
  * Date: 2017/12/5 13:42
- * Description: 用户模块微服务
+ * Description: 注册中心
  */
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class ServiceUserApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceUserApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 }
