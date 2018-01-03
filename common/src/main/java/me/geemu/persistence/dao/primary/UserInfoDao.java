df1fa35582ecb8a787fecc3671638e352d42b423
chenfangming <chenfangming@91zdan.com>
2018-01-03 09:33:40 +0800

    1

--- common/src/main/java/me/geemu/persistence/dao/primary/UserInfoDao.java
+++
@@ -1,7 +0,0 @@
-package me.geemu.persistence.dao.primary;
-
-import me.geemu.persistence.model.primary.UserInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserInfoDao extends Mapper<UserInfo> {
-}
