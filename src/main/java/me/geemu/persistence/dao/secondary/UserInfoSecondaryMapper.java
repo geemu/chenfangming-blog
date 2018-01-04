9dd35579cde3d667317261174382bf76b1826f91
陈方明 <cfmmail@sina.com>
2018-01-04 15:47:20 +0800

    1

--- src/main/java/me/geemu/persistence/dao/secondary/UserInfoSecondaryMapper.java
+++ src/main/java/me/geemu/persistence/dao/secondary/UserInfoSecondaryMapper.java
@@ -1,14 +1,14 @@
 package me.geemu.persistence.dao.secondary;
 
 import me.geemu.persistence.model.secondary.UserInfoSecondary;
+import me.geemu.util.MyMapper;
 import org.apache.ibatis.annotations.Param;
-import tk.mybatis.mapper.common.Mapper;
 
-public interface UserInfoSecondaryMapper extends Mapper<UserInfoSecondary> {
+public interface UserInfoSecondaryMapper extends MyMapper<UserInfoSecondary> {
     /**
      * @param account  账号
      * @param password 密码
      * @return 用户
      */
-    UserInfoSecondary findByAccountAndPassword(@Param("account") String account, @Param("password") String password);
+    UserInfoSecondary findByAccountAndPassword(@Param("account") String account, String password);
 }
