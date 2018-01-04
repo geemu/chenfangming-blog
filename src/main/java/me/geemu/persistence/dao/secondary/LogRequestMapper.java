32504d5591d4bde78307e5b38351f4eef268f805
陈方明 <cfmmail@sina.com>
2018-01-04 14:18:03 +0800

    s

--- src/main/java/me/geemu/persistence/dao/secondary/LogRequestMapper.java
+++
@@ -1,7 +0,0 @@
-package me.geemu.persistence.dao.secondary;
-
-import me.geemu.persistence.model.secondary.LogRequest;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface LogRequestMapper extends Mapper<LogRequest> {
-}
