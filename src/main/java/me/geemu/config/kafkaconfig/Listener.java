9f8567e3009a186763c531f4182cf59e4b8ffdfe
陈方明 <cfmmail@sina.com>
2018-01-04 12:48:03 +0800

    1

---
+++ src/main/java/me/geemu/config/kafkaconfig/Listener.java
@@ -0,0 +1,10 @@
+package me.geemu.config.kafkaconfig;
+
+/**
+ * @author Geemu
+ * Email: cfmmail@sina.com
+ * Date: 2018/1/4  11:25
+ * Description: Kafka 监听
+ */
+public class Listener {
+}
--- src/main/java/me/geemu/controller/UserInfoController.java
+++ src/main/java/me/geemu/controller/UserInfoController.java
@@ -1,20 +1,16 @@
 package me.geemu.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import me.geemu.domain.request.ChangePasswordRequest;
 import me.geemu.domain.response.LoginResponse;
 import me.geemu.service.IUserInfoService;
 import me.geemu.service.IUserServiceSecondary;
-import me.geemu.util.RequestUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
- * @Author: Geemu
- * Email:cfmmail@sina.com
+ * @uthor: Geemu
+ * Email: cfmmail@sina.com
  * Date: 2017/12/5 10:38
  * Description:
  */
