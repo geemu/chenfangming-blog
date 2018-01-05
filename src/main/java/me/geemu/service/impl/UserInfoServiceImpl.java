32b6bdbae81904701bc658062e183ae415f90248
陈方明 <cfmmail@sina.com>
2018-01-05 15:34:46 +0800

    1

--- src/main/java/me/geemu/service/impl/UserInfoServiceImpl.java
+++ src/main/java/me/geemu/service/impl/UserInfoServiceImpl.java
@@ -1,11 +1,19 @@
 package me.geemu.service.impl;
 
 import me.geemu.config.JwtConfig;
+import me.geemu.exception.NotFoundException;
 import me.geemu.persistence.dao.primary.UserInfoMapper;
+import me.geemu.persistence.model.primary.UserInfo;
+import me.geemu.persistence.model.primary.UserInfoExample;
 import me.geemu.service.UserInfoService;
+import me.geemu.util.AccessToken;
+import me.geemu.util.JwtUtil;
 import me.geemu.util.RedisUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author Geemu
@@ -16,6 +24,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserInfoServiceImpl implements UserInfoService {
 
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+    @Autowired
+    private RedisUtil redisUtil;
+    @Autowired
+    private JwtConfig jwtConfig;
 
     /**
      * @param account  根据账号密码查找用户 账号
@@ -24,7 +38,20 @@ public class UserInfoServiceImpl implements UserInfoService {
      */
     @Override
     public String findByAccountAndPassword(String account, String password) {
-        return "";
+        UserInfoExample example = new UserInfoExample();
+        example.createCriteria()
+                .andAccountEqualTo(account)
+                .andPasswordEqualTo(password)
+        ;
+        List<UserInfo> existList = userInfoMapper.selectByExample(example);
+        if (CollectionUtils.isEmpty(existList)) {
+            throw new NotFoundException("用户名或密码错误");
+        }
+        UserInfo currentUser = existList.get(0);
+        String token = JwtUtil.createJWT(new AccessToken(currentUser.getId(), currentUser.getAccount(), currentUser.getPassword()), jwtConfig);
+        // 将token写入redis
+        redisUtil.put("login_user:" + token, currentUser, jwtConfig.getExpiresSecond());
+        return token;
     }
 
 }
